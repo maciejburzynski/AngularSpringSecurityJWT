54981c98a4f3aa17aae4f761cb61ba86ee2da1bf
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-05-20 21:03:53 +0200

    Init spring project

---
+++ src/main/java/pl/maltoza/maltozasecurityjwt/MaltozaSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package pl.maltoza.maltozasecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaltozaSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaltozaSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maltoza/maltozasecurityjwt/MaltozaSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maltoza.maltozasecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaltozaSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
