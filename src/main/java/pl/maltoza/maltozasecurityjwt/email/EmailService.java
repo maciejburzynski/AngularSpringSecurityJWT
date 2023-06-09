52a7981068625743695f0340b299ee69c371d6a8
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-06-09 13:02:02 +0200

    Add mail functionality

---
+++ src/main/java/pl/maltoza/maltozasecurityjwt/email/EmailService.java
@@ -0,0 +1,35 @@
+package pl.maltoza.maltozasecurityjwt.email;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+@Service
+@RequiredArgsConstructor
+@Order(2)
+public class EmailService {
+
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(String to,
+                         String subject,
+                         String text,
+                         File file,
+                         boolean isHtmlContent) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(text, isHtmlContent);
+        mimeMessageHelper.addAttachment(file.getName(), file);
+        javaMailSender.send(mimeMessage);
+    }
+}
